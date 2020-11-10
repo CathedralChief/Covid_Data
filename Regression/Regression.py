125eb62ca4699beaae7255fb84a55598ba94bede
CathedralChief <70826529+CathedralChief@users.noreply.github.com>
2020-11-09 20:41:30 -0500

    Add actual regression scripts

--- Regression/Regression.py
+++ Regression/Regression.py
@@ -3,8 +3,11 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import csv
 import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
 # %%
 
+# Generate Cases DF based off of the csv
 school_list = []
 covid_cases = []
 
@@ -20,6 +23,25 @@ cases.close()
 
 combined_list = [school_list, covid_cases]
 
-df = pd.DataFrame(combined_list).transpose()
+case_df = pd.DataFrame(combined_list).transpose()
+case_df.columns = ['School', 'Cases']
+
+# Generate the values DF. Because everything is already formatted in the csv, can just import 
+# with pandas
+
+values_df = pd.read_csv('../School_Values.csv')
+
+df = case_df.merge(values_df, left_on = 'School', right_on='School')
+df = df.filter(['School', 'Cases', 'Activity Values', 'Safety Value'])
+
+# %%
+
+# Create the variables
+school = df['School']
+cases = df['Cases']
+activity = df['Activity Values']
+safety = df['Safety Value']
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(cases.astype('int'), activity)
 
 # %%
